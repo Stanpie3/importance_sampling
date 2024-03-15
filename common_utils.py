8ac45c5308936fadcf73fbbed25b0a4f6040ecb1
svk <savvakonst@gmail.com>
2024-03-15 11:28:42 +0300

    preliminary splitting for modules and some fixes in train

---
+++ common_utils.py
@@ -0,0 +1,49 @@
+class Average():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.count = 0
+        self.sum = 0
+        self.avg = 0
+
+    def update(self, val, n=1):
+        assert(n > 0)
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / float(self.count)
+
+
+
+class CallBack:
+    def __init__(self, eval_fn, name=None):
+        self.eval_fn = eval_fn
+        self.train_losses = []
+        self.train_accs = []
+        self.train_w_losses = []
+        self.train_max_p_i = []
+        self.train_num_unique_points = []
+        self.val_losses = []
+        self.val_accs = []
+        self.n_un = []
+
+    def last_info(self):
+        return {'loss_train': f'{self.train_losses[-1]:.3f}',
+                'acc_train': f'{self.train_accs[-1]:.3f}',
+                'w_loss_train': f'{self.train_w_losses[-1]:.3f}',
+                'loss_val': f'{self.val_losses[-1]:.3f}',
+                'acc_val': f'{self.val_accs[-1]:.3f}',
+                'n_un': f'{self.n_un[-1]:.3f}',
+        }
+    def __call__(self, model, val_dataloader, loss_fn,
+                 epoch_loss=None, epoch_acc=None, epoch_weighted_loss=None, epoch_max_p_i_s=None, epoch_num_unique_points_s=None, n_un=None):
+        self.train_losses.append(epoch_loss)
+        self.train_accs.append(epoch_acc)
+        self.train_w_losses.append(epoch_weighted_loss)
+        self.train_max_p_i.append(epoch_max_p_i_s)
+        self.train_num_unique_points.append(epoch_num_unique_points_s)
+        loss_val, acc_val = self.eval_fn(model, val_dataloader, loss_fn)
+        self.val_losses.append(loss_val)
+        self.val_accs.append(acc_val)
+        self.n_un.append(n_un)
+        return self.last_info()
---
+++ torch_svrg.py
@@ -0,0 +1,98 @@
+from torch_svrg_utils import SVRG_k ,SVRG_Snapshot, test_epoch_SVRG, train_epoch_SVRG
+
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
+
+mean, std = [0.5, 0.5, 0.5] , [0.5, 0.5, 0.5]
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20),
+    transforms.ToTensor(), 
+    transforms.Normalize(mean, std) 
+])
+test_transform = transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize(mean, std)
+])
+
+train_set = CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
+
+test_set = CIFAR10(root='./data', train=False, download=True, transform=test_transform) 
+test_loader = DataLoader(test_set, batch_size=32, shuffle=False)
+
+
+### END Solution (do not delete this comment)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+print('Train size', len(train_set))
+print('Test size', len(test_set))
+
+
+
+n_epochs = 20
+
+device = "cuda"
+
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CNN, self).__init__()
+        self.features = nn.Sequential(
+            # BEGIN Solution (do not delete this comment!)
+
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True), 
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)   
+
+            # END Solution (do not delete this comment!)
+        )
+        self.classifier = nn.Sequential(
+            # BEGIN Solution (do not delete this comment!)
+            nn.Linear(64 * 4 * 4, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.3),
+            nn.Linear(512, num_classes)
+            # END Solution (do not delete this comment!)
+        )
+
+    def forward(self, x):
+        # BEGIN Solution (do not delete this comment!)
+        
+        return self.classifier(self.features(x).view(x.size(0), -1) )
+        # END Solution (do not delete this comment!)
+
+
+
+model = CNN().to(device)
+model_snapshot = CNN().to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = SVRG_k(model.parameters(), lr=0.001, weight_decay = 0.0001)
+optimizer_snapshot = SVRG_Snapshot(model_snapshot.parameters())
+
+for epoch in tqdm(range(n_epochs)):
+    train_loss, train_acc = train_epoch_SVRG(model, model_snapshot, optimizer , optimizer_snapshot, train_loader, loss_fn )
+    test_loss, test_acc = test_epoch_SVRG(model, test_loader,  loss_fn)
+    print(f'[Epoch {epoch + 1}] train loss: {train_loss:.3f}; train acc: {train_acc:.2f}; ' + 
+          f'test loss: {test_loss:.3f}; test acc: {test_acc:.2f}')
