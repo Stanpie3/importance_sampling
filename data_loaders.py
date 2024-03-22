11ec661c4d23588f3231fa13cdbc331fef86ef61
svk <savvakonst@gmail.com>
2024-03-22 13:38:56 +0300

    add presample eqivalent to schaul

--- data_loaders.py
+++ data_loaders.py
@@ -1,9 +1,10 @@
 
 
+from typing import Iterable, Iterator, List, Sized, Union
 import torch
 from torchvision.datasets import CIFAR10, MNIST, CIFAR100, VisionDataset
 from torchvision import transforms
-from torch.utils.data.sampler import SubsetRandomSampler, RandomSampler
+from torch.utils.data.sampler import SubsetRandomSampler, RandomSampler, Sampler
 from torch.utils.data import DataLoader
 from PIL import Image
 
@@ -28,6 +29,48 @@ def addIndexes(data_set: type[VisionDataset]) :
             return index, img, target
     
     return IndexWrrapper
+
+
+
+class RnadomBatchSampler(Sampler[List[int]]):
+    r"""Sample real random batches.
+
+    Args:
+
+        data_source (Dataset): dataset to sample from
+        batch_size (int): Size of mini-batch.
+        replacement (bool): samples are drawn on-demand with replacement if ``True``, default=``False``
+        generator (Generator): Generator used in sampling.
+
+    """
+
+    def __init__(self, dataset: Sized, batch_size: int, replacement = False, generator = None) -> None:
+        if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
+                batch_size <= 0:
+            raise ValueError(f"batch_size should be a positive integer value, but got batch_size={batch_size}")
+
+        self._replacement = replacement
+        self._dataset_size = len(dataset)
+        self._batch_size = batch_size
+        self._generator = generator
+        self._unif = torch.ones(self.dataset_size)
+
+
+    def __iter__(self) -> Iterator[List[int]]:
+        n = self.__len__()
+
+        for _ in range(n):
+            batch = self._unif.multinomial(self._batch_size, 
+                                           replacement=self._replacement, 
+                                           generator= self._generator)
+            yield batch
+        #except StopIteration:
+
+    def __len__(self) -> int:
+        n = self._dataset_size // self._batch_size
+        n += (self._dataset_size % self._batch_size) > 0
+        return n
+
 
 
 
